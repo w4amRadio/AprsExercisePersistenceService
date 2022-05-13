57d2153f4263bbda861fac5a8ef0d41d3715c9aa
w4amRadio <daviddickey@w4am.net>
2022-05-13 01:24:33 -0400

    DBD: Script to sftp to pi to reduce repetitive tasks

---
+++ AprsPersistenceService/Utils/Deploy/aprs_persist_deploy.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+rm -r /home/pi/aprs_persist
+mkdir /home/pi/aprs_persist
+sleep 2s
+mv aprs_persist.tar /home/pi/aprs_persist/
+sleep 2s
+tar -xvf /home/pi/aprs_persist/aprs_persist.tar -C /home/pi/aprs_persist/
+rm /home/pi/aprs_persist/aprs_persist.tar
